f3200bf393708d2dfa4b5092df002411ac2247d2
Norbertohernadez <Jaquelinerosalesrosales@gmail.com>
2024-05-20 08:52:57 -0600

    Se agrego convertir tipo de datos

---
+++ Parcial1/2_Variables_Tipos_Datos/ConvertirTiposDatos.py
@@ -0,0 +1,37 @@
+#Convertir los tipos de datos
+
+#NOTA:Solo es posible en  una concatenacion concatenar entre tipos de datos iguales
+
+texto="soy una cadena"
+numero=23
+
+
+print(texto+" y soy cadena2")
+print(numero+34)
+
+#Convertir un tipo de dato int astr
+numero_str=str(numero)
+print(texto+" "+numero_str)
+
+prin(texto+" "+str(numero)) #Es la mas comun para mostrar una cadena con algo que no es una cadena 
+
+#Convertir un STR A INT
+
+n1=33
+suma=int('23')+n1
+print("la suma:"+str(suma))
+
+#Convertir un float a int 
+print( " " )
+n1=33
+n2=int(33.99)
+suma=n1+n2
+print(suma)
+
+#Convertir un int aa float 
+n1=3
+n2=4
+
+suma=n1+n2
+print(f"la suma es: {suma}")
+print("la suma es:"+str(suma))
