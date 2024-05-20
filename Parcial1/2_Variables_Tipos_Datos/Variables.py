7de89522dea5d821daf7ccd587b28fce64ed17b4
Norbertohernadez <Jaquelinerosalesrosales@gmail.com>
2024-05-20 09:11:06 -0600

    Se agrego variables

---
+++ Parcial1/2_Variables_Tipos_Datos/Variables.py
@@ -0,0 +1,25 @@
+#La variables son debilmente tipadas 
+#Es decir no es necesario indicar el tipo 
+#de dato al monento de utilizarlas
+
+#Variables
+edad=30
+estatura=1.80
+email="gay.com"
+logica=True
+caracter='@'
+
+#Imprimir contenido
+print(edad)
+print(estatura)
+print(email)
+print(logica)
+print(caracter)
+
+#Conocer el tipo de dato que python asigno
+print(type(edad))
+print(type(estatura))
+print(type(email))
+print(type(logica))
+print(type(caracter))
+print(" ")
