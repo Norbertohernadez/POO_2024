8e577555f8fd796afec0c2ca8acf326b28f9d3fd
Norbertohernadez <Jaquelinerosalesrosales@gmail.com>
2024-06-10 11:29:37 -0600

    Se corrigio nombre

---
+++ Parcial1/6_Ejercicios_Repaso_1al_10/Ejercicio1.py
@@ -0,0 +1,9 @@
+#Crear dos variables una pais y otra continente
+
+#Esta variable es de tipo texto
+Pais= 'Mexico'
+print ("El Pais es:", Pais) 
+
+#Esta variable es de tipo texto
+Continente= 'Americano'
+print("El continente es:", Continente) 
